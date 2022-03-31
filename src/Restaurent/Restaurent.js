59b12cbba28599193b5451a4c6743364344f739a
Sazzad ur Rahman <22joylobo@gmail.com>
2022-03-31 12:32:41 +0600

    Initiated

---
+++ src/Restaurent/Restaurent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Restaurent = () => {
+
+    
+    return (
+        <div>
+            <h3 className='text-xl mb-4 '>Search Your food</h3>
+            <input className='drop-shadow-xl border rounded-2xl border-rose-500' type="text" name="" id="" />
+        </div>
+    );
+};
+
+export default Restaurent;
